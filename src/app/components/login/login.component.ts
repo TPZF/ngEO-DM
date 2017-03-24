78e81d3228c299cc586a56d7a585dceb08f7ed7b
Maxime <maxime.shubin@telespazio.com>
2017-03-24 14:51:30 +0100

    Update login page with two steps: login -> dm registration or selection
    * Dirst step makes authetication request
    * Second allows user to connect to the available download managers or register a new download manager
    * TODO: Implement connection to LWS

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AuthenticationService } from './../../services/authentication.service';
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
@@ -9,43 +10,73 @@ import { NgeoService } from './../../services/ngeo';
   selector: 'login',
   template: `
 
-    <md-grid-list cols="1" rowHeight="300px">
-      <md-grid-tile>
-        <md-card>
-          <md-card-title>Login</md-card-title>
-          <md-card-content>
+    <md-tab-group class="login" [selectedIndex]="step">
+      <md-tab>
+        <md-grid-list cols="1" rowHeight="300px">
+          <md-grid-tile>
+            <md-card>
+              <md-card-title>Login</md-card-title>
+              <md-card-content>
 
-            <md-input-container class="full-width">
-              <input mdInput [(ngModel)]="userName" placeholder="Username">
-            </md-input-container>
+                <md-input-container class="full-width">
+                  <input mdInput [(ngModel)]="username" placeholder="Username">
+                </md-input-container>
 
-            <md-input-container class="full-width">
-              <input mdInput type="password" [(ngModel)]="password" placeholder="Password">
-            </md-input-container>
-
-            <md-input-container class="full-width">
-              <input mdInput [(ngModel)]="downloadManagerName" placeholder="Download manager name">
-            </md-input-container>
-
-          </md-card-content>
-        </md-card>
-      </md-grid-tile>
-      <md-grid-tile>
-        <button md-raised-button color="primary" (click)="login()">Login</button>
-        <button style="margin: 0px 10px" md-raised-button color="primary" (click)="register()">Register</button>
-        <md-progress-spinner style="display: inline-block; height: 16px; width: 16px;" *ngIf="isProcessing" mode="indeterminate"></md-progress-spinner>
-      </md-grid-tile>
-    </md-grid-list>
+                <md-input-container class="full-width">
+                  <input mdInput type="password" [(ngModel)]="password" placeholder="Password">
+                </md-input-container>
+              </md-card-content>
+            </md-card>
+          </md-grid-tile>
+          <md-grid-tile>
+            <button md-raised-button color="primary" (click)="login()">Login</button>
+            <md-progress-spinner style="display: inline-block; height: 16px; width: 16px;" *ngIf="isLoading" mode="indeterminate"></md-progress-spinner>
+          </md-grid-tile>
+        </md-grid-list>
+      </md-tab>
+      <md-tab>
+        <md-grid-list cols="2" rowHeight="300px">
+          <md-grid-tile>
+            <md-card>
+              <md-card-title>Select download manager</md-card-title>
+              <md-card-content>
+                <md-select color="primary" [(ngModel)]="selectedDownloadManager" *ngIf="downloadManagers.length > 0" placeholder="Download managers">
+                  <md-option *ngFor="let dm of downloadManagers" [value]="dm.downloadManagerFriendlyName">{{ dm.downloadManagerFriendlyName }}</md-option>
+                </md-select>
+                <button [disabled]="!selectedDownloadManager" style="margin: 0px 10px" md-raised-button color="primary" (click)="select()">Select</button>
+              </md-card-content>
+            </md-card>
+          </md-grid-tile>
+          <md-grid-tile>
+            <md-card>
+              <md-card-title>Register a new download manager</md-card-title>
+              <md-card-content>
+                <md-input-container class="full-width">
+                  <input mdInput [(ngModel)]="downloadManagerName" placeholder="Download manager name">
+                </md-input-container>
+                <button [disabled]="!downloadManagerName" style="margin: 0px 10px" md-raised-button color="primary" (click)="register()">Register</button>
+              </md-card-content>
+            </md-card>
+          </md-grid-tile>
+          <md-grid-tile colspan="2">
+            <button md-raised-button color="primary" (click)="logout()">Logout</button>
+          </md-grid-tile>
+        </md-grid-list>
+      </md-tab>
+    </md-tab-group>
   `,
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  private userName: string = "";
+  private step: number = 0;
+  private username: string = "";
   private password: string = "";
+  private isLoading: boolean = false;
   private downloadManagerName: string = "";
-  private isProcessing: boolean = false;
-  constructor(private _ngeoService: NgeoService, private router: Router) {
+  private downloadManagers = [];
+  private selectedDownloadManager;
+  constructor(private _ngeoService: NgeoService, private router: Router, private authenticationService: AuthenticationService) {
   }
 
   ngOnInit() {
@@ -54,7 +85,28 @@ export class LoginComponent implements OnInit {
   /**
    * Login user to entered download manager
    */
-  login() {
+  private login() {
+    this.isLoading = true;
+    this.authenticationService.login(this.username, this.password).subscribe((data) => {
+      this._ngeoService.getDownloadManagers(this.username).subscribe((downloadManagersRes) => {
+        this.step = 1;
+        this.isLoading = false;
+        this.downloadManagers = downloadManagersRes['downloadmanagers'];
+      })
+    });
+  }
+
+  private logout() {
+    this.authenticationService.logout().subscribe(() => {
+      this.step = 0;
+    });
+  }
+
+  /**
+   * Select the available download manager
+   */
+  private select() {
+    console.log(this.selectedDownloadManager);
     this.router.navigate(['/home']);
   }
 
--- src/app/services/ngeo.ts
+++ src/app/services/ngeo.ts
@@ -1,16 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class NgeoService {
-  baseUrl: string = 'http://localhost:3000/ngeo/catalogue/Landsat57Merged/search'
+  private baseUrl: string = 'http://localhost:3000/ngeo'
   constructor(private _http: Http) {
   }
 
   /**
-   * Just a request
+   * Just a simple search request
    */
-  retrieveSomething() {
-    return this._http.get(this.baseUrl).map((res) => { return res.json() });
+  public retrieveSomething() {
+    return this._http.get(this.baseUrl + '/catalogue/Landsat57Merged/search').map((res) => {
+      return res.json()
+    });
+  }
+
+  /**
+   * Get registered download managers for the given user
+   * @param username
+   */
+  public getDownloadManagers(username: string) {
+    // TODO: Make a real request to LWS
+    return Observable.of({
+      "downloadmanagers": [
+        {
+          "downloadManagerId": "DM_01",
+          "downloadManagerFriendlyName": "Magellium Limited Main DM 1",
+          "userId": "esa_user1",
+          "status": "ACTIVE",
+          "ipAddress": "dmServer.magellium.fr",
+          "lastAccessDate": "2001-12-17T09:30:47-05:00"
+        },
+
+        {
+          "downloadManagerId": "DM_02",
+          "downloadManagerFriendlyName": "TPZ Limited Main DM 2",
+          "userId": "tpz_user1",
+          "status": "INACTIVE",
+          "ipAddress": "dmServer.tpz.fr",
+          "lastAccessDate": "2012-11-22T09:30:47-05:00"
+        }
+      ]
+    });
+    // return this._http.get(this.baseUrl + '/downloadManagers').map((res) => {
+    //   return res.json();
+    // })
   }
 }
