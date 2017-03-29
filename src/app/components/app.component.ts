d491ae608117466344248f9f9942fd252963c0de
Manoel <manoel.olivier@gmail.com>
2017-03-29 15:02:39 +0200

    Use templateUrl for app component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -6,17 +6,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app',
   styleUrls: ['./app.component.scss'],
-  template: `
-    <div [class.m2app-dark]="isDarkTheme">
-        <main>
-            <router-outlet></router-outlet>
-            <br/>
-            <md-slide-toggle (change)="isDarkTheme = !isDarkTheme" [checked]="isDarkTheme" color="primary">
-                Set Dark theme
-            </md-slide-toggle>
-        </main>
-    </div>
-    `
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
 
