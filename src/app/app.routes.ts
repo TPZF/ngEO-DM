81f32277bb3ea4b14ecd97fcf0e1a7caa7f89b6e
Manoel <manoel.olivier@gmail.com>
2017-03-29 15:28:44 +0200

    Add search button in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { SearchComponent } from './components/search/search.component';
 import { downloadManagerRoutes } from './components/downloadManager/download-manager.routes';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'search', component: SearchComponent },
   ...downloadManagerRoutes
 ];
 
