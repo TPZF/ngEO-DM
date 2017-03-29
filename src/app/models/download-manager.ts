f0bd1aa3ded7d81e63baa5554f752c0e3f692986
Manoel <manoel.olivier@gmail.com>
2017-03-29 15:10:40 +0200

    Add model for downloadmanager

---
+++ src/app/models/download-manager.ts
@@ -0,0 +1,8 @@
+export class DownloadManager {
+  downloadManagerId: string;
+  downloadManagerFriendlyName: string;
+  userId: string;
+  status: string;
+  ipAddress: string;
+  lastAccessDate: string;
+};
