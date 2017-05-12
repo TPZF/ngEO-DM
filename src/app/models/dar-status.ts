9461921d4112fbd6a8505cddf2285e02bfa08312
Manoel <manoel.olivier@gmail.com>
2017-05-12 10:45:42 +0200

    Set localPath attribute for productStatus and downloaDirectory for DAR

--- src/app/models/dar-status.ts
+++ src/app/models/dar-status.ts
@@ -1,15 +1,17 @@
 export class ProductStatus {
-    expectedSize: string;
-    percentageCompleted: string;
-    productStatus: string;
-    productURL: string;
+  expectedSize: string;
+  percentageCompleted: string;
+  productStatus: string;
+  productURL: string;
+  localPath: string;
 };
 
 export class DarStatus {
-    ID: string;
-    dlManagerId: string;
-    message: string;
-    productStatuses: ProductStatus[];
-    status: number;
-    type: string;
+  ID: string;
+  dlManagerId: string;
+  message: string;
+  productStatuses: ProductStatus[];
+  status: number;
+  type: string;
+  downloadDirectory: string;
 };
