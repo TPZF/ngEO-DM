fb70df3b10022399c13fbc254ccb545ec5acaf31
Maxime Shubin <mshubin@Maximes-MacBook-Pro.local>
2017-06-13 18:06:39 +0200

    Changed the log file path to be located defaulted by electron-logger

--- src/electron/main.js
+++ src/electron/main.js
@@ -13,7 +13,11 @@ const log = require('electron-log');
 log.transports.file.level = 'all';
 log.transports.file.format = '{h}:{i}:{s}:{ms} {text}';
 log.transports.file.maxSize = 5 * 1024 * 1024;
-log.transports.file.file = 'ngeo-dm-log.log';
+/*by default it puts:
+on Linux: ~/.config/<app name>/log.log
+on OS X: ~/Library/Logs/<app name>/log.log
+on Windows: %USERPROFILE%\AppData\Roaming\<app name>\log.log*/
+//log.transports.file.file = 'ngeo-dm-log.log';
 
 log.debug('isDev=' + isDev);
 
