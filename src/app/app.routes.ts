2fe60c1481561b602997bf33993d1e6b5089833c
Manoel <manoel.olivier@gmail.com>
2017-06-16 09:04:13 +0200

    Remove Login and add direct download on routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
-import { ModuleWithProviders }  from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SettingsComponent } from './components/settings/settings.component';
-import { LoginComponent } from './components/login/login.component';
 import { SearchComponent } from './components/search/search.component';
+import { DirectDownloadComponent } from './components/directDownload/direct-download.component';
 import { downloadManagerRoutes } from './components/downloadManager/download-manager.routes';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/settings', pathMatch: 'full' },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'search', component: SearchComponent },
-  ...downloadManagerRoutes
+	{ path: '', redirectTo: '/settings', pathMatch: 'full' },
+	{ path: 'settings', component: SettingsComponent },
+	{ path: 'search', component: SearchComponent },
+	{ path: 'directDownload', component: DirectDownloadComponent },
+	...downloadManagerRoutes
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
