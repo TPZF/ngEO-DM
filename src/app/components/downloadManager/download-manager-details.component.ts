b8f9ab0618e4f44865a89533a0609eccd3393119
Manoel <manoel.olivier@gmail.com>
2017-06-19 15:37:28 +0200

    Add delete download manager and add DAR buttons / services

--- src/app/components/downloadManager/download-manager-details.component.ts
+++ src/app/components/downloadManager/download-manager-details.component.ts
@@ -1,38 +1,73 @@
 // Imports
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { DownloadManagerService } from './../../services/download-manager.service';
 import { DownloadManager } from './../../models/download-manager';
+import { DarStatusService } from './../../services/dar-status.service';
 
 @Component({
-  selector: 'download-manager-details',
-  templateUrl: './download-manager-details.component.html',
-  styleUrls: ['./download-manager-details.component.scss']
+	selector: 'download-manager-details',
+	templateUrl: './download-manager-details.component.html',
+	styleUrls: ['./download-manager-details.component.scss']
 })
 // Component class implementing OnInit
-export class DownloadManagerDetailsComponent implements OnInit {
-  // Private properties for binding
-  private sub: any;
-  
-  private downloadManager: DownloadManager;
+export class DownloadManagerDetailsComponent implements OnDestroy, OnInit {
+	// Private properties for binding
+	private _sub: any;
 
-  constructor(private downloadManagerService: DownloadManagerService, private route: ActivatedRoute) {
-  }
+	private downloadManager: DownloadManager;
 
-  // Load data ones component is ready
-  ngOnInit() {
-      // Subscribe to route params
-      this.sub = this.route.params.subscribe(params => {
-        let id = params['id'];
-       // Retrieve item with Id route param
-        this.downloadManagerService.getDownloadManager(id).subscribe(downloadManager => this.downloadManager = downloadManager);
-    });
-  }
+	constructor(
+		private _downloadManagerService: DownloadManagerService,
+		private _darStatusService: DarStatusService,
+		private _route: ActivatedRoute,
+		private _router: Router) {
+	}
 
-  ngOnDestroy() {
-      // Clean sub to avoid memory leak
-    this.sub.unsubscribe();
-  }
+	// Load data ones component is ready
+	ngOnInit() {
+		// Subscribe to route params
+		this._sub = this._route.params.subscribe(params => {
+			let id = params['id'];
+			// Retrieve item with Id route param
+			this._downloadManagerService.getDownloadManager(id).subscribe(_downloadManager => this.downloadManager = _downloadManager);
+		});
+	}
+
+	ngOnDestroy() {
+		// Clean sub to avoid memory leak
+		this._sub.unsubscribe();
+	}
+
+	private delete() {
+		this._downloadManagerService
+			.deleteDownloadManager(this.downloadManager.downloadManagerId)
+			.subscribe(_resp => {
+				if (_resp) {
+					this._router.navigate(['/downloadManagers']);
+				}
+			});
+	}
+
+	private addDARs() {
+		let myDar = {
+			simpledataaccessrequest: {
+				createBulkOrder: false,
+				requestStage: 'confirmation',
+				downloadLocation: {
+					DownloadManagerId: this.downloadManager.downloadManagerId,
+					DownloadDirectory: '/home/omanoel/Documents/dev/tmp/'
+				},
+				productURLs: [
+					{ productURL: 'https://eodata-service.user.eocloud.eu/eodata/MSI/L1C/2015/07/06/S2A_OPER_PRD_MSIL1C_PDMC_20160607T050846_R051_V20150706T105015_20150706T105015.SAFE/HTML/star_bg.jpg' },
+					{ productURL: 'https://eodata-service.user.eocloud.eu/eodata/MSI/L1C/2015/07/06/S2A_OPER_PRD_MSIL1C_PDMC_20160607T050846_R051_V20150706T105015_20150706T105015.SAFE/HTML/star_bg.jpg' },
+					{ productURL: 'https://eodata-service.user.eocloud.eu/eodata/MSI/L1C/2015/07/06/S2A_OPER_PRD_MSIL1C_PDMC_20160607T050846_R051_V20150706T105015_20150706T105015.SAFE/HTML/star_bg.jpg' }
+				],
+				name: 'SDAR_002'
+			}
+		};
+		this._darStatusService.addSimpleDAR(myDar);
+	}
 }
--- src/app/services/download-manager.service.ts
+++ src/app/services/download-manager.service.ts
@@ -12,67 +12,76 @@ import { DownloadManager } from './../models/download-manager';
 @Injectable()
 export class DownloadManagerService {
 
-  public currentDownloadManager = {};
+	public currentDownloadManager = {};
 
-  private baseUrl: string;
-  private downloadManagersUrl: string;
+	private baseUrl: string;
+	private downloadManagersUrl: string;
 
-  private headers = new Headers({ 'Content-Type': 'application/json' });
+	private headers = new Headers({ 'Content-Type': 'application/json' });
 
-  /**
-   * @function constructor
-   * @param http
-   */
-  constructor(
-    private http: Http,
-    private _authenticationService: AuthenticationService,
-    private _configurationService: ConfigurationService,
-    private _errorService: ErrorService) {
+	/**
+	 * @function constructor
+	 * @param http
+	 */
+	constructor(
+		private _http: Http,
+		private _authenticationService: AuthenticationService,
+		private _configurationService: ConfigurationService,
+		private _errorService: ErrorService) {
 		this.baseUrl = _configurationService.get().qsHost;
 		this.downloadManagersUrl = this.baseUrl + '/downloadManagers';
 	}
 
-  /**
-   * @function getDownloadManagers
-   */
-  getDownloadManagers(): Observable<DownloadManager[]> {
-    return this.http
-    .get(this.downloadManagersUrl)
-    .map((response) => {
-      return response.json().downloadmanagers;
-    })
-    .catch(this._errorService.handleError);
-  }
+	/**
+	 * @function getDownloadManagers
+	 */
+	getDownloadManagers(): Observable<DownloadManager[]> {
+		return this._http
+			.get(this.downloadManagersUrl)
+			.map((response) => {
+				return response.json().downloadmanagers;
+			})
+			.catch(this._errorService.handleError);
+	}
 
-  /**
-   * @function getDownloadManager
-   * @param id
-   */
-  getDownloadManager(id: string): Observable<DownloadManager> {
-    return this.http
-    .get(`${this.downloadManagersUrl}/${id}`)
-    .map(response => response.json().downloadmanager)
-    .catch(this._errorService.handleError);
-  }
+	/**
+	 * @function getDownloadManager
+	 * @param id
+	 */
+	getDownloadManager(id: string): Observable<DownloadManager> {
+		return this._http
+			.get(`${this.downloadManagersUrl}/${id}`)
+			.map(response => response.json().downloadmanager)
+			.catch(this._errorService.handleError);
+	}
 
-  /**
-   * Register a new download manager
-   */
-  public registerDownloadManager(downloadManagerInsert: any) {
+	/**
+	 * Register a new download manager
+	 */
+	public registerDownloadManager(downloadManagerInsert: any) {
 
-    let userId = this._authenticationService.getCurrentUser().username;
+		let userId = this._authenticationService.getCurrentUser().username;
 
-    let itemToAdd: DownloadManager = {
-      downloadManagerFriendlyName: downloadManagerInsert.downloadManagerFriendlyName,
-      userId: userId,
-      status: 'ACTIVE',
-      ipAddress: 'localhost',
-      lastAccessDate: Date.now().toString()
-    };
+		let itemToAdd: DownloadManager = {
+			downloadManagerFriendlyName: downloadManagerInsert.downloadManagerFriendlyName,
+			userId: userId,
+			status: 'ACTIVE',
+			ipAddress: 'localhost',
+			lastAccessDate: Date.now().toString()
+		};
 
-    return this.http
-      .post(this.downloadManagersUrl, {downloadmanager: itemToAdd})
-      .map((res) => res.json());
-  }
+		return this._http
+			.post(this.downloadManagersUrl, { downloadmanager: itemToAdd })
+			.map((res) => res.json());
+	}
+
+	deleteDownloadManager(myId: string): Observable<Boolean> {
+		return this._http
+			.delete(`${this.downloadManagersUrl}/${myId}`)
+			.map(response => {
+				return true;
+			})
+			.catch(this._errorService.handleError);
+	}
 
 }
