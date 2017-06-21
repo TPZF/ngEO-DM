01d3619d252a4c31d12564905ce20e933e02fc03
Manoel <manoel.olivier@gmail.com>
2017-06-21 16:39:13 +0200

    Add component dar-status-add in download manager details

--- src/app/components/downloadManager/download-manager-details.component.ts
+++ src/app/components/downloadManager/download-manager-details.component.ts
@@ -16,6 +16,7 @@ import { DarStatusService } from './../../services/dar-status.service';
 export class DownloadManagerDetailsComponent implements OnDestroy, OnInit {
 	// Private properties for binding
 	private _sub: any;
+	private _displayAdd: Boolean = false;
 
 	private downloadManager: DownloadManager;
 
@@ -51,23 +52,4 @@ export class DownloadManagerDetailsComponent implements OnDestroy, OnInit {
 			});
 	}
 
-	private addDARs() {
-		let myDar = {
-			simpledataaccessrequest: {
-				createBulkOrder: false,
-				requestStage: 'confirmation',
-				downloadLocation: {
-					DownloadManagerId: this.downloadManager.downloadManagerId,
-					DownloadDirectory: '/home/omanoel/Documents/dev/tmp/'
-				},
-				productURLs: [
-					{ productURL: 'https://eodata-service.user.eocloud.eu/eodata/MSI/L1C/2015/07/06/S2A_OPER_PRD_MSIL1C_PDMC_20160607T050846_R051_V20150706T105015_20150706T105015.SAFE/HTML/star_bg.jpg' },
-					{ productURL: 'https://eodata-service.user.eocloud.eu/eodata/MSI/L1C/2015/07/06/S2A_OPER_PRD_MSIL1C_PDMC_20160607T050846_R051_V20150706T105015_20150706T105015.SAFE/HTML/star_bg.jpg' },
-					{ productURL: 'https://eodata-service.user.eocloud.eu/eodata/MSI/L1C/2015/07/06/S2A_OPER_PRD_MSIL1C_PDMC_20160607T050846_R051_V20150706T105015_20150706T105015.SAFE/HTML/star_bg.jpg' }
-				],
-				name: 'SDAR_002'
-			}
-		};
-		this._darStatusService.addSimpleDAR(myDar);
-	}
 }
