7be1d2eb91cbbba9bafbae44901753b77a167f04
Manoel <manoel.olivier@gmail.com>
2017-06-26 15:37:50 +0200

    Format code

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -4,34 +4,34 @@ import { ElectronService } from 'ngx-electron';
 @Injectable()
 export class SettingsService {
 
-  constructor(private _electronService: ElectronService) {
+	constructor(private _electronService: ElectronService) {
 
-  }
+	}
 
-  /**
-   * @function get
-   * @param {string} myKey
-   * @returns {Object}
-   */
-  get(myKey: string): any {
-	return this._electronService.ipcRenderer.sendSync('settings-get', myKey);
-  }
+	/**
+	 * @function get
+	 * @param {string} myKey
+	 * @returns {Object}
+	 */
+	get(myKey: string): any {
+		return this._electronService.ipcRenderer.sendSync('settings-get', myKey);
+	}
 
-  /**
-   * @function getAll
-   * @returns {Object}
-   */
-  getAll(): any {
-	return this._electronService.ipcRenderer.sendSync('settings-getall', 'e');
-  }
+	/**
+	 * @function getAll
+	 * @returns {Object}
+	 */
+	getAll(): any {
+		return this._electronService.ipcRenderer.sendSync('settings-getall', 'e');
+	}
 
-  /**
-   * @function set
-   * @param {string} myKey
-   * @param {string} myValue
-   * @returns {void}
-   */
-  set(myKey: string, myValue: any): void {
-	  this._electronService.ipcRenderer.sendSync('settings-set', myKey, myValue);
-  }
+	/**
+	 * @function set
+	 * @param {string} myKey
+	 * @param {string} myValue
+	 * @returns {void}
+	 */
+	set(myKey: string, myValue: any): void {
+		this._electronService.ipcRenderer.sendSync('settings-set', myKey, myValue);
+	}
 }
